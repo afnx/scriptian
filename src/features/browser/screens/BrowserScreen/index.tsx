7efdc80dc783400a502cfc1382099b2e4c36f132
afnx <afndeveloper@gmail.com>
2025-10-27 00:33:50 -0700

    feat: refactor bottom container animation and style handling in BrowserScreen

--- src/features/browser/screens/BrowserScreen/index.tsx
+++ src/features/browser/screens/BrowserScreen/index.tsx
@@ -55,20 +55,6 @@ export default function BrowserScreen() {
     outputRange: [KEYBOARD_CLOSED_HEIGHT, KEYBOARD_OPENED_HEIGHT],
   });
 
-  const addressBarStyle = useMemo(
-    () => [
-      {
-        transform: [
-          {
-            translateY: Animated.add(height, offset),
-          },
-        ],
-      },
-      { backgroundColor: "white" },
-    ],
-    [height, offset]
-  );
-
   const bottomContainerY = useMemo(() => new Animated.Value(0), []);
 
   useEffect(() => {
@@ -78,6 +64,23 @@ export default function BrowserScreen() {
       useNativeDriver: true,
     }).start();
   }, [showBottomContainer, bottomContainerY]);
+
+  const bottomContainerStyle = useMemo(
+    () => [
+      {
+        transform: [
+          {
+            translateY: Animated.add(height, offset),
+          },
+          {
+            translateY: bottomContainerY,
+          },
+        ],
+      },
+      { backgroundColor: "white" },
+    ],
+    [height, offset, bottomContainerY]
+  );
 
   return (
     <View style={{ flex: 1 }}>
@@ -96,25 +99,11 @@ export default function BrowserScreen() {
         </View>
       </SafeAreaView>
 
-      <Animated.View
-        style={[
-          styles.bottomContainer,
-          {
-            backgroundColor: "white",
-            transform: [{ translateY: bottomContainerY }],
-            position: "absolute",
-            left: 0,
-            right: 0,
-            bottom: 0,
-          },
-        ]}
-      >
-        <Animated.View style={addressBarStyle}>
-          {showAddressBar && (
-            <AddressBar onFocusChange={onAddressBarFocusChange} />
-          )}
-          {!showAddressBar && <AddressBarDisplay />}
-        </Animated.View>
+      <Animated.View style={[styles.bottomContainer, bottomContainerStyle]}>
+        {showAddressBar && (
+          <AddressBar onFocusChange={onAddressBarFocusChange} />
+        )}
+        {!showAddressBar && <AddressBarDisplay />}
         {/* Toolbar contains the SafeAreaView */}
         <Toolbar />
       </Animated.View>
@@ -132,5 +121,10 @@ const styles = StyleSheet.create({
   },
   bottomContainer: {
     width: "100%",
+    backgroundColor: "white",
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
